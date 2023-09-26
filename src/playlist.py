e455f6a9cab60cf0616bb4ac421ee141f7cbaa59
AleksandrStrulkov <astrulkov@yandex.ru>
2023-09-26 22:18:21 +0300

    Add files

---
+++ src/playlist.py
@@ -0,0 +1,12 @@
+
+class Playlist:
+	def __init__(self, id_playlist):
+		self.id_playlist = id_playlist
+		self.name = None
+		self.url_playlist = None
+
+	def total_duration(self):
+		pass
+
+	def show_best_video(self):
+		pass
